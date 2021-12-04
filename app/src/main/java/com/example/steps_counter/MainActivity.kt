ebe8e56fd1638f216fc9156ace26895b2287401d
levailev <levailevente84@gmail.com>
2021-12-04 15:21:55 +0100

    Sensor manager implementálása

--- app/src/main/java/com/example/steps_counter/MainActivity.kt
+++ app/src/main/java/com/example/steps_counter/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.steps_counter
 
+import android.content.Context
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var running = false
+    var sensorManager: SensorManager? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
     }
 }
